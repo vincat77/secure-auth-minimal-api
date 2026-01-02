19adeb8ecb8f885befa75b38558dfda718bf6e89
vincat77 <vincenzo.catrambone@gmail.com>
2026-01-02 05:27:08 +0100

    WinForms: ActionButtonsControl separato in Designer compatibile VS

--- clients/WinFormsClient/Controls/ActionButtonsControl.cs
+++ clients/WinFormsClient/Controls/ActionButtonsControl.cs
@@ -3,60 +3,11 @@ using System.Windows.Forms;
 
 namespace WinFormsClient.Controls;
 
-/// <summary>
-/// UserControl con i pulsanti di azione principali e la checkbox Remember.
-/// </summary>
-public sealed class ActionButtonsControl : UserControl
+public sealed partial class ActionButtonsControl : UserControl
 {
-    private readonly Button _registerButton = new() { Text = "Registrati", Width = 155, Height = 30 };
-    private readonly Button _confirmEmailButton = new() { Text = "Conferma email", Width = 155, Height = 30 };
-    private readonly Button _loginButton = new() { Text = "Login (password)", Width = 155, Height = 30 };
-    private readonly Button _confirmMfaButton = new() { Text = "Conferma MFA", Width = 155, Height = 30 };
-    private readonly Button _refreshButton = new() { Text = "Refresh", Width = 155, Height = 30 };
-    private readonly Button _setupMfaButton = new() { Text = "Attiva MFA", Width = 155, Height = 30 };
-    private readonly Button _disableMfaButton = new() { Text = "Disattiva MFA", Width = 155, Height = 30 };
-    private readonly Button _meButton = new() { Text = "Mostra profilo", Width = 155, Height = 30 };
-    private readonly Button _logoutButton = new() { Text = "Logout", Width = 155, Height = 30 };
-    private readonly Button _showQrButton = new() { Text = "Mostra QR MFA", Width = 155, Height = 30 };
-    private readonly CheckBox _rememberCheck = new() { Text = "Ricordami", AutoSize = true };
-
     public ActionButtonsControl()
     {
-        Height = 320;
-        Width = 180;
-        // Posizionamento verticale fisso.
-        int x = 0;
-        int y = 0;
-        int step = 35;
-
-        _registerButton.Location = new System.Drawing.Point(x, y); y += step;
-        _confirmEmailButton.Location = new System.Drawing.Point(x, y); y += step;
-        _loginButton.Location = new System.Drawing.Point(x, y); y += step;
-        _confirmMfaButton.Location = new System.Drawing.Point(x, y); y += step;
-        _refreshButton.Location = new System.Drawing.Point(x, y); y += step;
-        _setupMfaButton.Location = new System.Drawing.Point(x, y); y += step;
-        _disableMfaButton.Location = new System.Drawing.Point(x, y); y += step;
-        _meButton.Location = new System.Drawing.Point(x, y); y += step;
-        _logoutButton.Location = new System.Drawing.Point(x, y); y += step;
-        _showQrButton.Location = new System.Drawing.Point(x, y); y += step;
-        _rememberCheck.Location = new System.Drawing.Point(x, y + 5);
-
-        Controls.AddRange(new Control[]
-        {
-            _registerButton,_confirmEmailButton,_loginButton,_confirmMfaButton,_refreshButton,_setupMfaButton,
-            _disableMfaButton,_meButton,_logoutButton,_showQrButton,_rememberCheck
-        });
-
-        _registerButton.Click += (s, e) => RegisterClicked?.Invoke(this, EventArgs.Empty);
-        _confirmEmailButton.Click += (s, e) => ConfirmEmailClicked?.Invoke(this, EventArgs.Empty);
-        _loginButton.Click += (s, e) => LoginClicked?.Invoke(this, EventArgs.Empty);
-        _confirmMfaButton.Click += (s, e) => ConfirmMfaClicked?.Invoke(this, EventArgs.Empty);
-        _refreshButton.Click += (s, e) => RefreshClicked?.Invoke(this, EventArgs.Empty);
-        _setupMfaButton.Click += (s, e) => SetupMfaClicked?.Invoke(this, EventArgs.Empty);
-        _disableMfaButton.Click += (s, e) => DisableMfaClicked?.Invoke(this, EventArgs.Empty);
-        _meButton.Click += (s, e) => MeClicked?.Invoke(this, EventArgs.Empty);
-        _logoutButton.Click += (s, e) => LogoutClicked?.Invoke(this, EventArgs.Empty);
-        _showQrButton.Click += (s, e) => ShowQrClicked?.Invoke(this, EventArgs.Empty);
+        InitializeComponent();
     }
 
     public event EventHandler? RegisterClicked;
@@ -69,6 +20,21 @@ public sealed class ActionButtonsControl : UserControl
     public event EventHandler? MeClicked;
     public event EventHandler? LogoutClicked;
     public event EventHandler? ShowQrClicked;
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        _registerButton.Click += (s, _) => RegisterClicked?.Invoke(this, EventArgs.Empty);
+        _confirmEmailButton.Click += (s, _) => ConfirmEmailClicked?.Invoke(this, EventArgs.Empty);
+        _loginButton.Click += (s, _) => LoginClicked?.Invoke(this, EventArgs.Empty);
+        _confirmMfaButton.Click += (s, _) => ConfirmMfaClicked?.Invoke(this, EventArgs.Empty);
+        _refreshButton.Click += (s, _) => RefreshClicked?.Invoke(this, EventArgs.Empty);
+        _setupMfaButton.Click += (s, _) => SetupMfaClicked?.Invoke(this, EventArgs.Empty);
+        _disableMfaButton.Click += (s, _) => DisableMfaClicked?.Invoke(this, EventArgs.Empty);
+        _meButton.Click += (s, _) => MeClicked?.Invoke(this, EventArgs.Empty);
+        _logoutButton.Click += (s, _) => LogoutClicked?.Invoke(this, EventArgs.Empty);
+        _showQrButton.Click += (s, _) => ShowQrClicked?.Invoke(this, EventArgs.Empty);
+    }
 
     public bool RememberChecked
     {
