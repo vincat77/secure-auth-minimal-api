141eac3647ea5714ae7a237bf08e48337b762041
vincat77 <vincenzo.catrambone@gmail.com>
2026-01-03 23:56:34 +0100

    commit generale

--- clients/WinFormsClient/ImageLoader.cs
+++ clients/WinFormsClient/ImageLoader.cs
@@ -1,46 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Net.Http;
+using Svg;
 
-namespace WinFormsClient
+namespace WinFormsClient;
+
+public static class ImageLoader
 {
-  using System.Drawing;
-  using System.Net.Http;
-  using Svg;
-
-  public static class ImageLoader
-  {
-    private static readonly HttpClient http = new();
+    private static readonly HttpClient Http = new();
 
     public static async Task<Image> LoadFromUrlAsync(string url, int width, int height)
     {
-      await using var stream = await http.GetStreamAsync(url);
+        await using var stream = await Http.GetStreamAsync(url);
 
-      if (IsSvg(url))
-        return LoadSvg(stream, width, height);
+        if (IsSvg(url))
+            return LoadSvg(stream, width, height);
 
-      return LoadRaster(stream);
+        return LoadRaster(stream);
     }
 
     private static Image LoadSvg(Stream stream, int width, int height)
     {
-      var doc = SvgDocument.Open<SvgDocument>(stream);
-      using var bmp = doc.Draw(width, height);
-      return (Image)bmp.Clone();
+        var doc = SvgDocument.Open<SvgDocument>(stream);
+        using var bmp = doc.Draw(width, height);
+        return (Image)bmp.Clone();
     }
 
     private static Image LoadRaster(Stream stream)
     {
-      return Image.FromStream(stream);
+        return Image.FromStream(stream);
     }
 
     private static bool IsSvg(string url)
     {
-      return url.EndsWith(".svg", StringComparison.OrdinalIgnoreCase)
-             || url.Contains("svg");
+        return url.EndsWith(".svg", StringComparison.OrdinalIgnoreCase) || url.Contains("svg");
     }
-  }
-
 }
